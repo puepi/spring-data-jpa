834dbc97c0ab8a99becdaff3a8f1924c43c249e4
puepi <pmandel0001@yahoo.fr>
2025-05-15 19:04:42 +0100

    new new changes

---
+++ src/main/java/databases/entity/Teacher.java
@@ -0,0 +1,27 @@
+package databases.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class Teacher {
+    @Id
+    @SequenceGenerator(name = "teacher_sequence", sequenceName = "teacher_sequence", allocationSize = 1)
+    @GeneratedValue(generator = "teacher_sequence", strategy = GenerationType.SEQUENCE)
+    private Long teacherId;
+    private String firstName;
+    private String lastName;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "teacher_id",referencedColumnName = "teacherId")
+    List<Course> courses;
+}
